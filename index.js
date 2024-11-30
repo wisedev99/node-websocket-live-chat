f175cda5764ef4f1a6196a92fdf3f4097aa2458f
Aliakbar Boistov <wisedev2699@gmail.com>
2024-12-01 01:00:22 +0500

    added first step for websoket in  node js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const webserver = express()
+    .use((req, res) =>
+        res.sendFile('/websocket-client.html', { root: __dirname })
+    )
+    .listen(3000, () => console.log(`Listening on ${3000}`))
+
+
+const { WebSocketServer } = require('ws')
+const sockserver = new WebSocketServer({ port: 443 })
+
+sockserver.on('connection', ws => {
+    console.log('New client connected!')
+    ws.send('Welcome to the WebSocket server!');
+
+    ws.on('close', () => console.log('Client has disconnected!'))
+    ws.on('message', data => {
+        sockserver.clients.forEach(client => {
+            console.log(`message frome client : ${data}`)
+            client.send(`hello dear client thi is you mesage ----${data}`)
+        })
+    })
+    ws.onerror = function () {
+        console.log('websocket error')
+    }
+})
